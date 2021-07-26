9db4cc19f80fd2c62135e6824e171984e1ed9754
InToSlapCrap <43097389+InToSlapCrap@users.noreply.github.com>
2021-07-26 21:47:30 +0200

    Delete IntermissionSystem directory

--- IntermissionSystem/src/client/init.client.lua
+++
@@ -1,5 +0,0 @@
-local Knit = require(game:GetService("ReplicatedStorage").Knit)
-
-Knit.AddControllers(script.Parent.Controllers)
-
-Knit.Start():Catch(warn)
--- IntermissionSystem/src/server/init.server.lua
+++
@@ -1,6 +0,0 @@
-local Knit = require(game:GetService("ReplicatedStorage").Knit)
-
-
-Knit.AddServices(script.Parent.Services)
-
-Knit.Start():Catch(warn)
